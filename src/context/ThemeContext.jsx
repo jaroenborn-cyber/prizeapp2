fe2ccde096a6a26e513eb265ec8233b80ade3794
Jaroen Born <mail@jborn.nl>
2025-12-22 02:04:02 +0700

    Fix: Export setTheme from ThemeContext to enable theme switching in fullscreen mode

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -27,7 +27,7 @@ export const ThemeProvider = ({ children }) => {
   };
 
   return (
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+    <ThemeContext.Provider value={{ theme, setTheme, toggleTheme }}>
       {children}
     </ThemeContext.Provider>
   );
